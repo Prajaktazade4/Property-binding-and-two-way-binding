f050f1335da80e6f8fce00bc22d97b35c5cc67c1
prajakta zade <prajaktazade0@gmail.com>
2023-12-16 21:50:49 +0530

    Binding project

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'taskTen';
+  heading='PROPERTY BINDING'
+  itemImageUrl = '/assets/tshirt.avif';
+  data:any;
+  data1:string='this is AnkHub'
+  disable=true;
+  enableBox(){
+    this.disable=false;
+  }
+}
